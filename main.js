d010180496465b2e4cee7f511af9e6751ea420c7
Ivan Pesenti <ivan.pesenti@abenergie.it>
2021-03-26 13:23:01 +0100

    first release

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
 var wordToGuess = '';
-var errorCount = 0;
+var errorCount = -1;
 
 function play() {
+    if (document.getElementById('result-section').hasChildNodes()) {
+        document.getElementById('result-section').removeChild(document.getElementById('result-section').childNodes[0]);
+    }
     var parola = document.getElementById('word').value;
     if (parola === null || parola === '') {
         alert('inserisci una parola da indovinare');
     } else {
         wordToGuess = parola.toLowerCase();
-        var playArea = document.getElementById('play-area');
         var gamePlay = document.getElementById('game-play');
         for (const char of parola) {
             var charNode = document.createElement('span');
@@ -15,11 +17,19 @@ function play() {
             charNode.classList.add('letter-' + char);
             gamePlay.appendChild(charNode);
         }
-        var img = document.createElement('img');
+        printLetters();
+    }
+}
 
-        img.src = './img/' + errorCount + '.jpg';
-        img.id = 'hanged-img';
-        playArea.appendChild(img);
+function printLetters() {
+    var lettersGroup = document.getElementById('letters-group');
+    const letters = 'abcdefghijklmnopqrstuvwxyz'.split('');
+    for (const letter of letters) {
+        var btn = document.createElement('button');
+        btn.innerHTML = letter;
+        btn.style.marginRight = "6px";
+        btn.addEventListener('click', () => tryLetter(letter));
+        lettersGroup.appendChild(btn);
     }
 }
 
@@ -40,6 +50,28 @@ function tryLetter(letter) {
                 child.innerHTML = letter;
             }
         });
+
+        // check if the user won
+        var gamePlay = document.getElementById('game-play');
+        var spanInGamePlay = gamePlay.getElementsByTagName('span');
+        var missingLetter = 0;
+        for (const span of spanInGamePlay) {
+            if (span.innerText === '_')
+                missingLetter++;
+        }
+        if (missingLetter === 0) {
+            // you won
+            var resultNode = document.createElement('p');
+            resultNode.innerHTML = "You win!";
+            var resultSection = document.getElementById('result-section');
+            resultSection.appendChild(resultNode);
+            document.getElementById('button-section').getElementsByTagName('button')[0].disabled = true;
+            var letters = document.getElementById('letters-group');
+            var buttons = letters.getElementsByTagName('button');
+            for (const button of buttons) {
+                button.disabled = true;
+            }
+        }
     } else {
         errorCount++;
         if (errorCount === 6) {
@@ -48,25 +80,45 @@ function tryLetter(letter) {
             resultNode.innerHTML = "You Lose!";
             var resultSection = document.getElementById('result-section');
             resultSection.appendChild(resultNode);
-            reset();
+            document.getElementById('button-section').getElementsByTagName('button')[0].disabled = true;
+            var letters = document.getElementById('letters-group');
+            var buttons = letters.getElementsByTagName('button');
+            for (const button of buttons) {
+                button.disabled = true;
+            }
         } else {
-            document.getElementById('hanged-img').src = './img/' + errorCount + '.jpg';
+            // check if it's the first error made
+            if (errorCount === 0) {
+                var playArea = document.getElementById('play-area');
+                var img = document.createElement('img');
+                img.src = './img/' + errorCount + '.jpg';
+                img.id = 'hanged-img';
+                playArea.appendChild(img);
+            } else {
+                document.getElementById('hanged-img').src = './img/' + errorCount + '.jpg';
+            }
         }
 
     }
 }
 
 function reset() {
+    if (document.getElementById('result-section').hasChildNodes()) {
+        document.getElementById('result-section').removeChild(document.getElementById('result-section').childNodes[0]);
+    }
     document.getElementById('word').value = '';
     while (document.getElementById('game-play').hasChildNodes()) {
         document.getElementById('game-play').removeChild(document.getElementById('game-play').childNodes[0]);
     }
-    document.getElementById('play-area').removeChild(document.getElementById('play-area').childNodes[0]);
+    if (document.getElementById('play-area').hasChildNodes()) {
+        document.getElementById('play-area').removeChild(document.getElementById('play-area').childNodes[0]);
+    }
+    document.getElementById('button-section').getElementsByTagName('button')[0].disabled = false;
     var letters = document.getElementById('letters-group');
     var buttons = letters.getElementsByTagName('button');
     for (const button of buttons) {
         button.disabled = false;
     }
     wordToGuess = '';
-    errorCount = 0;
+    errorCount = -1;
 }
